c7b3015c2998cd6f56e7b3ac0ae2272e4d7f5441
Sunday Emijere (Business Guest) <emijere.richard@gmail.com>
2023-10-28 21:21:54 +0100

    feat: welcome message on the nav

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,8 +1,8 @@
-"use strict";
-import config from "./config.js";
+'use strict';
+import config from './config.js';
 
 // getting username from session storage
-const getUserName = () => sessionStorage.getItem("username") || "Unknown";
+const getUserName = () => sessionStorage.getItem('username') || 'Unknown';
 
 const connection = new signalR.HubConnectionBuilder()
   .withUrl(`${config.baseUrl}?username=${getUserName()}`) //May change based on your port number
@@ -12,7 +12,7 @@ const connection = new signalR.HubConnectionBuilder()
 const start = async () => {
   try {
     await connection.start();
-    console.log("SignalR Connected.");
+    console.log('SignalR Connected.');
   } catch (err) {
     console.log(err);
   }
@@ -23,7 +23,7 @@ const joinChat = async (user) => {
 
   try {
     const message = `${user} joined.`;
-    await connection.invoke("JoinChat", user, message);
+    await connection.invoke('JoinChat', user, message);
     console.log(message);
   } catch (error) {
     console.log(error);
@@ -37,8 +37,8 @@ const getNotificationMessage = async () => {
   if (!currentUser) return;
 
   try {
-    await connection.on("ReceiveMessage", (user, message) => {
-      const messageClass = user === currentUser ? "sent-msg" : "received-msg";
+    await connection.on('ReceiveMessage', (user, message) => {
+      const messageClass = user === currentUser ? 'sent-msg' : 'received-msg';
       appendMessage(message, messageClass);
     });
   } catch (error) {
@@ -48,17 +48,17 @@ const getNotificationMessage = async () => {
 
 // appending message to the message section
 const appendMessage = (message, messageClass) => {
-  const messageSection = document.getElementById("messageSection");
-  const messageElement = document.createElement("div");
-  messageElement.classList.add("msg-box");
+  const messageSection = document.getElementById('messageSection');
+  const messageElement = document.createElement('div');
+  messageElement.classList.add('msg-box');
   messageElement.classList.add(messageClass);
   messageElement.innerHTML = message;
   messageSection.appendChild(messageElement);
 };
 
 // binding the send button
-document.getElementById("sendBtn").addEventListener("click", async (e) => {
-  console.log("called");
+document.getElementById('sendBtn').addEventListener('click', async (e) => {
+  console.log('called');
   e.preventDefault();
 
   const user = getUserName();
@@ -67,31 +67,39 @@ document.getElementById("sendBtn").addEventListener("click", async (e) => {
   if (!user) return;
   // if (!user || !receiver) return;
 
-  const messageInputElement = document.getElementById("messageInput");
+  const messageInputElement = document.getElementById('messageInput');
   const messageInput = messageInputElement.value;
 
   if (messageInput) {
     // call method to send message
     await sendMessage(user, `${user}: ${messageInput}`); // send message to server
-    messageInputElement.value = "";
+    messageInputElement.value = '';
   }
 });
 
 const sendMessage = async (user, message) => {
   try {
-    await connection.invoke("SendMessage", user, message);
+    await connection.invoke('SendMessage', user, message);
   } catch (error) {
     console.log(error);
   }
 };
 
+const setUsername = () => {
+  const username = getUserName();
+  document.getElementById('usernamePlaceholder').textContent = username;
+};
+
 const startApp = async () => {
   await start();
+
   const user = getUserName();
   if (user) {
     await joinChat(user);
     await getNotificationMessage();
   }
+
+  setUsername(); // Set the username when the app starts
 };
 
 startApp();
--- client/js/chatList.js
+++ client/js/chatList.js
@@ -1,7 +1,7 @@
-"use strict";
-import config from "./config.js";
+'use strict';
+import config from './config.js';
 
-const getUserName = () => sessionStorage.getItem("username") || "Unknown";
+const getUserName = () => sessionStorage.getItem('username') || 'Unknown';
 
 const connection = new signalR.HubConnectionBuilder()
   .withUrl(`${config.baseUrl}?username=${getUserName()}`) //May change based on your port number
@@ -11,93 +11,99 @@ const connection = new signalR.HubConnectionBuilder()
 const start = async () => {
   try {
     await connection.start();
-    console.log("SignalR Connected.");
+    console.log('SignalR Connected.');
   } catch (err) {
     console.log(err);
   }
 };
 
 const appendUserChatBox = async () => {
-  const users = JSON.parse(sessionStorage.getItem("users"));
+  const users = JSON.parse(sessionStorage.getItem('users'));
   // const groups = JSON.parse(sessionStorage.getItem("groups"));
-  const usersSection = document.getElementById("users-section");
-  usersSection.innerHTML = "";
+  const usersSection = document.getElementById('users-section');
+  usersSection.innerHTML = '';
 
   if (users.length > 0) {
     users.forEach(function (user) {
       if (user.userId !== connection.connectionId) {
         // Create an anchor element
-        const a = document.createElement("a");
+        const a = document.createElement('a');
         // Set the attributes and the innerHTML
         a.setAttribute(
-          "href",
-          "/client/pages/chatSingle.html?receiver=" + user.userName
+          'href',
+          '/client/pages/chatSingle.html?receiver=' + user.userName
         );
-        a.setAttribute("class", "chat-box");
-        a.innerHTML = "<p>Chat with " + user.userName + "</p><p>></p>";
+        a.setAttribute('class', 'chat-box');
+        a.innerHTML = '<p>Chat with ' + user.userName + '</p><p>></p>';
         // Append the anchor element to the div element
         usersSection.appendChild(a);
       }
     });
   } else {
-    usersSection.innerHTML = "<p>No users found.</p>";
+    usersSection.innerHTML = '<p>No users found.</p>';
   }
 };
 
 const appendGroupChatBox = async () => {
-  const groups = JSON.parse(sessionStorage.getItem("groups"));
+  const groups = JSON.parse(sessionStorage.getItem('groups'));
   // const groups = JSON.parse(sessionStorage.getItem("groups"));
-  const groupsSection = document.getElementById("groups-section");
-  groupsSection.innerHTML = "";
+  const groupsSection = document.getElementById('groups-section');
+  groupsSection.innerHTML = '';
 
   if (groups.length > 0) {
     groups.forEach(function (group) {
       // Create an anchor element
-      const a = document.createElement("a");
+      const a = document.createElement('a');
       // Set the attributes and the innerHTML
-      a.setAttribute("href", "/client/pages/chatGroup.html?groupName=" + group);
-      a.setAttribute("class", "chat-box");
-      a.innerHTML = "<p>Chat in " + group + "</p><p>></p>";
+      a.setAttribute('href', '/client/pages/chatGroup.html?groupName=' + group);
+      a.setAttribute('class', 'chat-box');
+      a.innerHTML = '<p>Chat in ' + group + '</p><p>></p>';
       // Append the anchor element to the div element
       groupsSection.appendChild(a);
     });
   } else {
-    groupsSection.innerHTML = "<p>No groups found.</p>";
+    groupsSection.innerHTML = '<p>No groups found.</p>';
   }
 };
 
 const createGroup = async () => {
-  document.getElementById("create-group").addEventListener("click", (e) => {
+  document.getElementById('create-group').addEventListener('click', (e) => {
     e.preventDefault();
-    const groupName = prompt("What name would you like to name your group");
+    const groupName = prompt('What name would you like to name your group');
     if (groupName) {
-      connection.invoke("CreateGroup", groupName);
+      connection.invoke('CreateGroup', groupName);
     }
   });
 };
 
 const appLogic = async () => {
-  connection.invoke("RequestUsersAndGroups");
-  connection.on("ReceiveUserAndGroups", (users, groups) => {
+  connection.invoke('RequestUsersAndGroups');
+  connection.on('ReceiveUserAndGroups', (users, groups) => {
     console.log(users);
-    sessionStorage.setItem("users", JSON.stringify(users));
-    sessionStorage.setItem("groups", JSON.stringify(groups));
+    sessionStorage.setItem('users', JSON.stringify(users));
+    sessionStorage.setItem('groups', JSON.stringify(groups));
     appendUserChatBox();
     appendGroupChatBox();
   });
 
   createGroup();
 
-  connection.on("RoomAlreadyExists", (room) => {
-    alert("Group already exists with name: " + room);
+  connection.on('RoomAlreadyExists', (room) => {
+    alert('Group already exists with name: ' + room);
   });
-  connection.on("GroupCreated", (room) => {
-    alert("Group created successfully");
+  connection.on('GroupCreated', (room) => {
+    alert('Group created successfully');
   });
+};
+
+const setUsername = () => {
+  const username = getUserName();
+  document.getElementById('usernamePlaceholder').textContent = username;
 };
 
 const startApp = async () => {
   await start();
+  setUsername(); // Set the username when the app starts
 
   appLogic();
 };
